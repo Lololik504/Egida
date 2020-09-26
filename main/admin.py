347090449374c3439abaef96efd03eb231c0761c
lololik504 <lololik504@gmail.com>
2020-09-26 22:50:47 +0700

    commit

---
+++ main/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(District)
+admin.site.register(School)
+
+# admin.site.register(Entity)
+# admin.site.register(RepairWorks)
+# admin.site.register(Mandates)
+# admin.site.register(DeputiesOrders)
+# admin.site.register(Documentation)
+#
+admin.site.register(BuildingConstructions)
+# admin.site.register(EngineeringStructures)
+# admin.site.register(IndoorSpaces)
+# admin.site.register(SafetySystem)
+# admin.site.register(Landscaping)
+# admin.site.register(SportsFacilietes)
+admin.site.register(Temperatures)
---
+++ main/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+
+
+# Create your models here.
+
+# ----------------For Entity-------------------
+#
+# class RepairWorks(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class Mandates(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class DeputiesOrders(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class Documentation(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class Entity(models.Model):
+#     repair_works = models.ForeignKey(RepairWorks, on_delete=models.CASCADE)
+#     mandates = models.ForeignKey(Mandates, on_delete=models.CASCADE)
+#     deputies_orders = models.ForeignKey(DeputiesOrders, on_delete=models.CASCADE)
+#     documentation = models.ForeignKey(Documentation, on_delete=models.CASCADE)
+#     is_municipal = models.BooleanField('Муниципальное здание')
+#
+#
+# # ----------------For buildings characters-----------------
+
+
+class BuildingConstructions(models.Model):
+    title = models.CharField(max_length=100)
+
+
+# class EngineeringStructures(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class IndoorSpaces(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class SafetySystem(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class Landscaping(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+#
+# class SportsFacilietes(models.Model):
+#     title = models.CharField(max_length = 100)
+#
+
+class Temperatures(models.Model):
+    coolent_temp = models.IntegerField()
+    air_temp = models.IntegerField()
+
+
+# -----------------Schools and districts---------------
+
+
+class District(models.Model):
+    name = models.CharField(max_length=50)
+
+
+    class Meta:
+        verbose_name = "Район"
+        verbose_name_plural = "Районы"
+
+    def __str__(self):
+        return self.name
+
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, default=None)
+    building_constructions = models.ForeignKey(BuildingConstructions, on_delete=models.CASCADE, default=None)
+    temperatures = models.ForeignKey(Temperatures, on_delete=models.CASCADE, default=None)
+
+    #     engineering_structures = models.ForeignKey(EngineeringStructures, on_delete=models.CASCADE)
+    #     indoor_spaces = models.ForeignKey(IndoorSpaces, on_delete=models.CASCADE)
+    #     safety_system = models.ForeignKey(SafetySystem, on_delete=models.CASCADE)
+    #     landscaping = models.ForeignKey(Landscaping, on_delete=models.CASCADE)
+    #     sports_facilietes = models.ForeignKey(SportsFacilietes, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Школа"
+        verbose_name_plural = "Школы"
+
+    def __str__(self):
+        return self.name
