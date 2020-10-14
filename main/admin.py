aef3883156717ed21af712669de1f8473f0e09f6
lololik504 <lololik504@gmail.com>
2020-10-14 17:39:50 +0700

    try to do admin customs

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpResponseRedirect
+from django.urls import path
+
+from accounts import models as amodels
 from .models import *
 
 # Register your models here.
@@ -9,3 +14,23 @@ admin.site.register(Building)
 
 
 # admin.site.register(Temperatures)
+
+
+
+class YOUR_MODELAdmin(admin.ModelAdmin):
+    change_list_template = "admin/monitor_change_list.html"
+
+    @staff_member_required
+    def export(self, request):
+        print("EXPORT")
+        return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path("export/", self.export)
+        ]
+        return my_urls + urls
+
+
+# admin.site.register(amodels.AdminUser, YOUR_MODELAdmin)
