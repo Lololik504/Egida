8ed507d4af6b421c250458b5c5938827c933b7b4
lololik504 <lololik504@gmail.com>
2020-10-17 22:33:07 +0700

    ok

--- main/admin.py
+++ main/admin.py
@@ -5,3 +5,7 @@ from .models import *
 admin.site.register(District)
 admin.site.register(School)
 admin.site.register(Building)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/main/School/change_list.html'
