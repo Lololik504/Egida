488752fe76ef802d3f8e35a9cc9c424370205c6a
temakolpakov <temakolpakov@gmail.com>
2021-03-10 19:29:01 +0700

    CharField to TextField

--- main/models/building_construction/Roof.py
+++ main/models/building_construction/Roof.py
@@ -5,9 +5,9 @@ from main.MyModelFile import MyModel
 
 class Roof(MyModel):
     roof_square = models.FloatField(verbose_name="Площадь кровли", blank=True, null=True)
-    roof_type = models.CharField(verbose_name="Тип кровли", max_length=50, null=True, blank=True)
-    roof_material = models.CharField(verbose_name="Материал кровли", max_length=50, null=True, blank=True)
-    roof_status = models.CharField(verbose_name="Состояние кровли", max_length=50, null=True, blank=True)
+    roof_type = models.TextField(verbose_name="Тип кровли", null=True, blank=True)
+    roof_material = models.TextField(verbose_name="Материал кровли", null=True, blank=True)
+    roof_status = models.TextField(verbose_name="Состояние кровли", null=True, blank=True)
 
     class Meta:
         abstract = True
