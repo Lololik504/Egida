89d52ce0a8d14e1f63ac6f35eb8c73ed083d1725
Artem Kolpakov <temakolpakov@gmail.com>
2021-03-18 13:51:55 +0700

    Change some photos

--- main/models/building_construction/BlindArea.py
+++ main/models/building_construction/BlindArea.py
@@ -10,7 +10,8 @@ class BlindArea(MyModel):
     blind_area_length = models.FloatField(verbose_name="Длина отмостки", blank=True, null=True)
     blind_area_type = models.CharField(verbose_name="Тип отмостки", max_length=50, null=True, blank=True)
     blind_area_status = models.CharField(verbose_name="Состояние отмостки", max_length=50, null=True, blank=True)
-    blind_area = models.FileField(verbose_name="Акт обследования технического состояния", upload_to=inn_dir_path, default=None, null=True, blank=True)
+    blind_area_act = models.FileField(verbose_name="Акт обследования технического состояния", upload_to=inn_dir_path, default=None, null=True, blank=True)
+    blind_area_photo = models.FileField(verbose_name="Фото отмостки", upload_to=inn_dir_path, default=None, null=True, blank=True)
 
     class Meta:
         abstract = True
