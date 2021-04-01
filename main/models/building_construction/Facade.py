9025979e6775a22b70039cb5fc73bb8f2a9e3212
Artem Kolpakov <temakolpakov@gmail.com>
2021-04-01 13:15:51 +0700

    Changed types of Facade.py

--- main/models/building_construction/Facade.py
+++ main/models/building_construction/Facade.py
@@ -6,8 +6,8 @@ from .helpers import inn_dir_path
 class Facade(MyModel):
 
     facade_square = models.FloatField(verbose_name="Площадь фасада", blank=True, null=True)
-    facade_type = models.CharField(verbose_name="Тип фасада", max_length=50, null=True, blank=True)
-    facade_status = models.CharField(verbose_name="Состояние фасада", max_length=50, null=True, blank=True)
+    facade_type = models.TextField(verbose_name="Тип фасада", null=True, blank=True)
+    facade_status = models.TextField(verbose_name="Состояние фасада", null=True, blank=True)
     facade_photo = models.FileField(verbose_name="Фото фасада", upload_to=inn_dir_path, default=None, null=True, blank=True)
     facade_act = models.FileField(verbose_name="Акт обследования технического состояния", upload_to=inn_dir_path, default=None, null=True, blank=True)
 
